617b94a516f684eb59270afccd3a7cebec2389ab
Ashutosh Rana <26782153+mindquaker@users.noreply.github.com>
2021-03-11 11:43:21 -0500

    Delete UpdateLeader.js

--- src/UpdateLeader.js
+++
@@ -1,8 +0,0 @@
-export function  UpdateLeader(props){
-   
-   return(
-       <div >
-            
-            <button  className = "update-leader" onClick={()=>props.onUpdateLeader()}>Update Leaderboard</button>
-        </div>) 
-}
